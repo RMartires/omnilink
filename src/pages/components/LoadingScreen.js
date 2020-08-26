15631d2075dc653d028d77a6c6d9645e3ec10dbb
RMartires <rohitmartires14@gmail.com>
2020-08-27 04:41:05 +0530

    updated new loading screen

--- src/pages/components/LoadingScreen.js
+++ src/pages/components/LoadingScreen.js
@@ -1,78 +1,8 @@
 import React from "react";
-//import classes from './LoadingScreen.module.css'
+import classes from "./LoadingScreen.module.scss";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  "@keyframes spin3D": {
-    from: {
-      transform: "rotate3d(0deg)",
-    },
-    to: {
-      transform: "rotate3d(.5,.5,.5, 360deg)",
-    },
-  },
-  spinnerbox: {
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "transparent",
-  },
-
-  /* SPINNING CIRCLE */
-
-  leoborder1: {
-    position: "absolute",
-    width: "150px",
-    height: "150px",
-    padding: "3px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: "50%",
-    background: "rgb(63,249,220)",
-    background:
-      "linear-gradient(0deg, rgba(63,249,220,0.1) 33%, rgba(63,249,220,1) 100%)",
-    animation: "$spin3D 1.8s linear 0s infinite",
-  },
-
-  leocore1: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#37474faa",
-    borderRadius: "50%",
-  },
-
-  leoborder2: {
-    position: "absolute",
-    width: "150px",
-    height: "150px",
-    padding: "3px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: "50%",
-    background: "rgb(63,249,220)",
-    background:
-      "linear-gradient(0deg, rgba(255, 171, 145,0.1) 33%, rgba(255, 171, 145,1) 100%)",
-    animation: "$spin3D 2.2s linear 0s infinite",
-  },
-
-  leocore2: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#ffab91aa",
-    borderRadius: "50%",
-  },
-  "*": {
-    boxSizing: "border-box",
-  },
-}));
 
 const footer = (onlyspinner) => {
   if (!onlyspinner) {
@@ -102,21 +32,11 @@ const footer = (onlyspinner) => {
 };
 
 function LoadingScreen(props) {
-  const classes = useStyles();
-
   return (
     <Container fluid>
       <Row style={props.onlyspinner ? { height: "100vh" } : { height: "95vh" }}>
         <Col>
-          <div className={classes.spinnerbox}>
-            <div className={classes.leoborder1}>
-              <div className={classes.leocore1}></div>
-            </div>
-            <div className={classes.leoborder2}>
-              <div className={classes.leocore2}></div>
-            </div>
-            <p style={{ fontSize: "1.8rem" }}>Loading...</p>
-          </div>
+          <div class={classes.loader}>Loading...</div>
         </Col>
       </Row>
       {footer(props.onlyspinner)}
